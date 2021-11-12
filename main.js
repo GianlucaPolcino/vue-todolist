d250a22ac8033dada9890c1e12cfc81456cc2c11
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-11-12 15:37:16 +0100

    all milestone done

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 const app = new Vue ({
-    
+    el: '#app',
+
+    data:{
+        toDoList:[
+            {
+                object: 'Comprare il latte',
+                done: false,
+            },
+
+            {
+                object: 'Pagare le bollette',
+                done: false,
+            },
+
+            {
+                object: 'Conquistare il mondo',
+                done: false,
+            },
+        ],
+
+        toDo: {
+            object: '',
+            done: false,
+        },
+
+        error: false,
+
+        active:false,
+    },
+
+    methods:{
+        addTask(){
+            if(this.toDo < 1){
+                this.error = true;
+            } else{
+                this.toDoList.push(this.toDo);
+                this.toDo = {
+                    object: '',
+                    done: false,
+                };
+                this.error = false;
+            }
+        },
+
+        removeTask(index){
+            this.toDoList.splice(index, 1)
+        },
+
+        
+    }
 })
